94a5f1b22d955312d2be7368d92bce29d0956ec3
Hien Le <lequanghien247@gmail.com>
2021-06-07 22:00:21 +0200

    Add params for sensor

--- src/sensor_bridge_listener.cpp
+++ src/sensor_bridge_listener.cpp
@@ -13,15 +13,13 @@ class SensorBridgeListener : public rclcpp::Node {
 public:
     explicit SensorBridgeListener() : Node("sensor_bridge_listener") {
 
-        rmw_qos_profile_t qos_profile = rmw_qos_profile_default;
+        parse_parameters();
         auto qos = rclcpp::QoS(
                 rclcpp::QoSInitialization(
-                        qos_profile.history,
-                        qos_profile.depth
-                ),
-                qos_profile);
-
-        //qos.best_effort();
+                        history_policy_,
+                        depth_
+                ));
+        qos.reliability(reliability_policy_);
 
         // manually enable topic statistics via options
         auto options = rclcpp::SubscriptionOptions();
@@ -55,6 +53,58 @@ public:
 
 private:
     rclcpp::Subscription<bridge_msgs::msg::Sensor>::SharedPtr subscription_;
+    size_t depth_;
+    rmw_qos_reliability_policy_t reliability_policy_;
+    rmw_qos_history_policy_t history_policy_;
+    std::map<std::string, rmw_qos_reliability_policy_t> name_to_reliability_policy_map = {
+            {"reliable", RMW_QOS_POLICY_RELIABILITY_RELIABLE},
+            {"best_effort", RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT}
+    };
+
+    std::map<std::string, rmw_qos_history_policy_t> name_to_history_policy_map = {
+            {"keep_last", RMW_QOS_POLICY_HISTORY_KEEP_LAST},
+            {"keep_all", RMW_QOS_POLICY_HISTORY_KEEP_ALL}
+    };
+
+    void parse_parameters() {
+        // Parse 'reliability' parameter
+        rcl_interfaces::msg::ParameterDescriptor reliability_desc;
+        reliability_desc.description = "Reliability QoS setting for the listener";
+        reliability_desc.additional_constraints = "Must be one of: ";
+        for (auto entry : name_to_reliability_policy_map) {
+            reliability_desc.additional_constraints += entry.first + " ";
+        }
+        const std::string reliability_param = this->declare_parameter(
+                "reliability", "reliable", reliability_desc);
+        auto reliability = name_to_reliability_policy_map.find(reliability_param);
+        if (reliability == name_to_reliability_policy_map.end()) {
+            std::ostringstream oss;
+            oss << "Invalid QoS reliability setting '" << reliability_param << "'";
+            throw std::runtime_error(oss.str());
+        }
+        reliability_policy_ = reliability->second;
+
+        // Parse 'history' parameter
+        rcl_interfaces::msg::ParameterDescriptor history_desc;
+        history_desc.description = "History QoS setting for the listener";
+        history_desc.additional_constraints = "Must be one of: ";
+        for (auto entry : name_to_history_policy_map) {
+            history_desc.additional_constraints += entry.first + " ";
+        }
+        const std::string history_param = this->declare_parameter(
+                "history", name_to_history_policy_map.begin()->first, history_desc);
+        auto history = name_to_history_policy_map.find(history_param);
+        if (history == name_to_history_policy_map.end()) {
+            std::ostringstream oss;
+            oss << "Invalid QoS history setting '" << history_param << "'";
+            throw std::runtime_error(oss.str());
+        }
+        history_policy_ = history->second;
+
+        // Declare and get remaining parameters
+        depth_ = this->declare_parameter("depth", 10);
+    }
+
 };
 
 
